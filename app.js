174c99459f00b8037e446c897a80179efea8d222
Julia-Susser <you@example.com>
2020-08-11 18:56:50 -0700

    initial commit

--- app.js
+++ app.js
@@ -19,3 +19,10 @@ app.post('/url', function(req, res) {
         res.send('hey');
     });
 });
+
+const { calculateAverages, addClimateRowToObject, logNodeError, printQueryResults } = require('./utils');
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database('./db.sqlite');
+
+db.run('CREATE TABLE Average (id INTEGER PRIMARY KEY, year INTEGER NOT NULL, temperature REAL NOT NULL)', logNodeError);
