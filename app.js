753d26700a9ecef61159ad5530d0eacac6d03499
Julia-Susser <you@example.com>
2020-08-11 20:20:50 -0700

    initial commit

--- app.js
+++ app.js
@@ -5,33 +5,7 @@ const port = process.env.PORT || 3000;
 const urlShortener = require('node-url-shortener');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    var firebase = require("firebase-admin");
-
-    var serviceAccount = require("./serviceAccountKey.json");
-
-    firebase.initializeApp({
-      credential: firebase.credential.cert(serviceAccount),
-      databaseURL: "https://privado-1d138.firebaseio.com"
-    });
-
-    var db = firebase.database();
-    var ref = db.ref("restricted_access/secret_document");
-    ref.once("value", function(snapshot) {
-      console.log(snapshot.val());
-    });
-
-    var usersRef = ref.child("users");
-    usersRef.set({
-      alanisawesome: {
-        date_of_birth: "June 23, 1912",
-        full_name: "Alan Turing"
-      },
-      gracehop: {
-        date_of_birth: "December 9, 1906",
-        full_name: "Grace Hopper"
-      }
-    });
-
+    
 });
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
