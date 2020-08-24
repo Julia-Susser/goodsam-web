5e9e8376ebec382d3cfaf837c42e5ad30ce1253c
Julia-Susser <you@example.com>
2020-08-24 10:15:33 -0700

    make it better

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
 const url = require('url');
 
-const readline = require('readline');
-const {google} = require('googleapis');
+
 
 
 
