a4ebbeeb762eccfe20f2217f607b5e18959b5ab6
Julia-Susser <you@example.com>
2020-08-23 22:12:33 -0700

    make it better

---
+++ public/navbar.js
@@ -0,0 +1,31 @@
+
+
+
+
+document.write(
+  `
+
+  <div class="topnav">
+  <img id ="logo" src="../logo2.png">
+  <a class="nav_ref" href="/home">Home</a>
+  <a class="nav_ref" href="/donate">Donate</a>
+  <a class="nav_ref" href="/contactus">Contact Us</a>
+  <a class="nav_ref" href="/instagram">Instagram</a>
+
+
+    <div class="dropdown">
+      <p class="dropbtn">
+        Help Out
+      </p>
+
+      <div class="dropdown-content">
+      <a href="./Amazon-List">Volenteer</a>
+      <a href="./Wishlist">Wishlist</a>
+      <a href="">Amazon Wishlist</a>
+      </div>
+
+    </div>
+    <a class="nav_ref" id="logout" href="/logout">LogOut</a>
+    </div>
+    `
+)
